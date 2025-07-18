36f74ef90fa877aaa7774f94f0d0b342544e5d18
AnthonyCine <75588050+AnthonyCine@users.noreply.github.com>
2025-07-18 18:09:20 -0400

    Update urls.py

--- boredsearch_backend/boredsearch/urls.py
+++ boredsearch_backend/boredsearch/urls.py
@@ -1,14 +1,27 @@
 from django.urls import path
-from.views import home_page, privacy, dev_login, dev_feed, app_ads_txt, about, user_verification, delete_account
+from.views import Favorites, UserInterests, UserManagement, PasswordReset, VerificationProcess, BoredSearchFeed, \
+    Searches, DevUpload, Views, ContentEdits, EmailPasswordReset, get_home
 
 urlpatterns = [
-    path('app-ads.txt/', app_ads_txt),
-    path('mobile/boredsearch/privacy_page/', privacy),
-    path('privacy_page/', privacy),
-    path('about/', about),
-    path('verification/<mode>/<token>/<user_id>/', user_verification),
-    path('verification/resend/<mode>/<token>/<user_id>/', user_verification),
-    path('deleteaccount/', delete_account)
-    # path('user_login/', dev_login),
-    # path('bored/', dev_feed),
+    path('home/', get_home),
+    path('dev/<dev_code>/', DevUpload.as_view()),
+    path('edit/delete/<int:pk>/<all>/', ContentEdits.as_view()),
+    path('edit/edit/<int:pk>/<editing>/', ContentEdits.as_view()),
+    path('edit/pull/<sort>/', ContentEdits.as_view()),
+    #
+    path('users/dev/<dev_code>/', UserManagement.as_view()),
+    path('users/<user_status>/', UserManagement.as_view()),
+    path('change_password/', PasswordReset.as_view()),
+    path('reset_password/email_verification/<request_type>/', EmailPasswordReset.as_view()),
+    path('interests/<action>/', UserInterests.as_view()),
+    path('interests/', UserInterests.as_view()),
+    path('main_bored/<int:result_amount>/<search_types>/<interests>/', BoredSearchFeed.as_view()), ##add topics to pull
+    path('search/<keyword>/<search_type>/<direction>/<int:pg>/<safe>/', Searches.as_view()),
+    path('verify/new/account/', VerificationProcess.as_view()),
+    #
+    path('favorites/<request_type>/<folder_name>/', Favorites.as_view()), #get request
+    path('favorites/<request_type>/<folder_name>/<int:content_id>/', Favorites.as_view()), # post and delete request
+    #
+    path('views/', Views.as_view()),
+
 ]
