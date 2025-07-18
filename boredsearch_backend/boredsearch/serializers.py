ccb6bfee461f909ae82eb02d9c3f9abe1cf4d980
AnthonyCine <75588050+AnthonyCine@users.noreply.github.com>
2025-07-18 18:07:41 -0400

    Update serializers.py

--- boredsearch_backend/boredsearch/serializers.py
+++ boredsearch_backend/boredsearch/serializers.py
@@ -1,36 +1,24 @@
 from rest_framework import serializers
-from .models import Users, Contents, FavoriteContents, ViewedContents, AccountVerification, Folders
+from .models import Users, Contents, ViewedContents, Folders
 
 
 class UsersSerializers(serializers.ModelSerializer):
     class Meta:
         model = Users
-        fields = ['id', 'username', 'email', 'password', 'interests']
-
-
-class AccountVerificationSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = AccountVerification
-        fields = ['token', 'expiration', 'user']
+        fields = ['id', 'username', 'email', 'password', 'interests', 'is_active', 'is_superuser', 'is_staff']
 
 
 class ContentsSerializers(serializers.ModelSerializer):
     class Meta:
         model = Contents
         fields = ['id', 'title', 'link', 'topics', 'search_type', 'snippet', 'is_safe', 'source', 'posted_by',
-                  'aspect_ratio', 'thumbnail']
+                  'aspect_ratio', 'thumbnail', 'main_keyword']
 
 
 class FoldersSerializers(serializers.ModelSerializer):
     class Meta:
         model = Folders
-        fields = ['user', 'name']
-
-
-class FavoriteContentsSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = FavoriteContents
-        fields = ['user', 'content_id', 'folder']
+        fields = ['id', 'user', 'folders']
 
 
 class ViewedContentsSerializers(serializers.ModelSerializer):
