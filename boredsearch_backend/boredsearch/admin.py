33dd9ce87c534d3da1793ed5e3bbc829316ef586
AnthonyCine <75588050+AnthonyCine@users.noreply.github.com>
2025-07-18 18:04:39 -0400

    Update admin.py

--- boredsearch_backend/boredsearch/admin.py
+++ boredsearch_backend/boredsearch/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Users, Contents, AccountVerification, FavoriteContents, ViewedContents
+from .models import Users, Contents, ViewedContents
 
 admin.site.register(Users)
 admin.site.register(Contents)
-admin.site.register(AccountVerification)
-admin.site.register(FavoriteContents)
 admin.site.register(ViewedContents)
